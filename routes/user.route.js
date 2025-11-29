4f9635e899086a98daeed269f168925bfa455c56
Zurab <101800477+wedz0zur@users.noreply.github.com>
2025-11-29 13:33:17 +0300

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import { handleValidationErrors } from "../middlewares/error.validator.js";
-import { createResumeValidation, updateResumeValidation } from "../middlewares/validators/resume.validator.js";
-import { createResume, deleteResume, getResume, getResumes, getResumesCount, updateResume } from "../controllers/resume.controller.js";
-
+import userController from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/", getUsers);
-userRouter.get("/count", getUsersCount);
-userRouter.get("/:userId", getUser);
-userRouter.put("/:userId", authMiddleware, updateResumeValidation, handleValidationErrors, updateUser);
-userRouter.delete("/:userId", authMiddleware, deleteUser);
+// Публичные маршруты
+userRouter.get("/", userController.getUsers); //+
+userRouter.get("/count", userController.getUsersCount); //+
+userRouter.get("/:userId", userController.getUser); //+
+
+// Защищенные маршруты (требуют аутентификации)
+userRouter.get("/profile/me", authMiddleware, userController.getUserProfile); //+
+userRouter.put("/profile/me", authMiddleware, userController.updateUserProfile); //+
+userRouter.delete("/:userId", authMiddleware, userController.deleteUser); //+
 
 export default userRouter;
